5a9645123b70c6f1d0c1f6b1d0579fc8f3c7487d
linzheng <alidlg9045@126.com>
2022-10-07 20:38:23 +0800

    创建全局css , vite配置css变量的全局引用

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
 })
