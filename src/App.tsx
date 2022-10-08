c96e1f487dc43e92f764166b76ac449b2cbe6daf
linzheng <alidlg9045@126.com>
2022-10-08 14:14:42 +0800

    css module写法;PostCss集入;CSS原子化框架-unocss接入

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import { Header } from './components/Header';
+import "uno.css";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <p className="bg-red-400">Hello Vite + React!</p>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
+import autoprefixer from 'autoprefixer';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unocss({
+      presets: [
+        presetAttributify({ /* preset options */}),
+        presetUno(),
+      ],
+    })
+  ],
   css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
+    },
+    modules: {
+      // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
+      // 其中，name 表示当前文件名，local 表示类名
+      generateScopedName: "[name]__[local]___[hash:base64:5]"
+    },
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
