0f272a84da7aaab31579fd96e5226a72f50e0dbc
linzheng <alidlg9045@126.com>
2022-10-10 14:38:40 +0800

    接入 antfu 的eslint插件 & viteEslint 检查插件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-import { Header } from './components/Header';
-import "uno.css";
+import { Header } from './components/Header'
+import 'uno.css'
 import './App.css'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+import path from 'path'
 import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 import Unocss from 'unocss/vite'
 import { presetAttributify, presetUno } from 'unocss'
-import autoprefixer from 'autoprefixer';
+import autoprefixer from 'autoprefixer'
+import viteEslint from 'vite-plugin-eslint'
 
-const variablePath = normalizePath(path.resolve('./src/variable.scss'));
+const variablePath = normalizePath(path.resolve('./src/variable.scss'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,30 +14,31 @@ export default defineConfig({
     react(),
     Unocss({
       presets: [
-        presetAttributify({ /* preset options */}),
+        presetAttributify({ /* preset options */ }),
         presetUno(),
       ],
-    })
+    }),
+    viteEslint(),
   ],
   css: {
     postcss: {
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
     },
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
       // 其中，name 表示当前文件名，local 表示类名
-      generateScopedName: "[name]__[local]___[hash:base64:5]"
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`
-      }
-    }
-  }
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
 })
